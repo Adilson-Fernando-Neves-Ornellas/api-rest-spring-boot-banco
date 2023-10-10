dff55db0d7182c3e1a07679c1d59ffbdc4e6862b
Adilson Fernando <ornellas00@hotmail.com>
2023-10-10 10:56:09 -0300

    feat: criando a base do projeto, configurando a conexão com banco de dados

---
+++ bankdigital/src/main/java/com/bank/bankdigital/model/enderecoModel.java
@@ -0,0 +1,77 @@
+package com.bank.bankdigital.model;
+
+public class enderecoModel {
+    
+    private Long id;
+    private String rua;
+    private String cep;
+    private int  numeroCasa;
+    private String complemento;
+    private String logradouro;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String pais;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getRua() {
+        return rua;
+    }
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+    public String getCep() {
+        return cep;
+    }
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+    public int getNumeroCasa() {
+        return numeroCasa;
+    }
+    public void setNumeroCasa(int numeroCasa) {
+        this.numeroCasa = numeroCasa;
+    }
+    public String getComplemento() {
+        return complemento;
+    }
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+    public String getLogradouro() {
+        return logradouro;
+    }
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+    public String getBairro() {
+        return bairro;
+    }
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+    public String getCidade() {
+        return cidade;
+    }
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+    public String getEstado() {
+        return estado;
+    }
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    public String getPais() {
+        return pais;
+    }
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+}
---
+++ bankdigital/src/test/java/com/bank/bankdigital/BankdigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.bankdigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankdigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
